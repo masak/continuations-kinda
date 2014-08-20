e81aa7a5fe021484f22b823540bca2cf19bad7b9
Carl Masak <cmasak@gmail.com>
2014-08-20 17:33:15 +0200

    bake handling of "next continuation" into a method

--- lib/Continuations/Kinda.pm
+++ lib/Continuations/Kinda.pm
@@ -4,12 +4,27 @@ use JSON::Unmarshal;
 constant START = 0;
 constant FINISH = Inf;
 
+class X::Continuation::Finished is Exception {
+    method message { "Continuation is spent" }
+}
+
+class X::Continuation::NoDestinationSet is Exception {
+    method message { "No destination set" }
+}
+
 class Continuation {
     has $.n = START;
     has $.finished = False;
-    has $.next;
+    has $!next;
 
     method goto($!next) {
+    }
+
+    method next() {
+        die X::Continuation::NoDestinationSet.new
+            unless defined $!next;
+        my $finished = $!next == FINISH;
+        return Continuation.new(:n($!next), :$finished);
     }
 
     method save($file) {
@@ -31,24 +46,13 @@ my class Part {
     }
 }
 
-class X::Continuation::Finished is Exception {
-    method message { "Continuation is spent" }
-}
-
-class X::Continuation::NoDestinationSet is Exception {
-    method message { "No destination set" }
-}
-
 sub build_resumable(*@parts) is export {
     return sub (:$cont = Continuation.new) {
         die X::Continuation::Finished.new
             if $cont.finished;
         my $n = $cont.n;
         @parts[$n].run($cont);
-        die X::Continuation::NoDestinationSet.new
-            unless defined $cont.next;
-        my $finished = $cont.next == FINISH;
-        return Continuation.new(:n($cont.next), :$finished);
+        return $cont.next;
     };
 }
 
